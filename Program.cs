f5def20a18f139688f38cc2caa6a5b5d86ea2dba
praveenpdevops <88812913+praveenpdevops@users.noreply.github.com>
2021-08-25 22:29:04 -0500

    welcome to core project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jenkinsdotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!, hello ashiwn and praveen");
+        }
+    }
+}
